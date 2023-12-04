d5ad80a66be488f0233f9d6adb4630b0fbf29cec
Joita Razvan-Gabriel <joitarazvan@yahoo.com>
2023-12-04 13:47:17 +0200

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @SpringBootApplication
 @RestController
 public class Main {
