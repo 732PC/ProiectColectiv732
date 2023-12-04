cc72dcb530d365b00af57418a6adbbce90675839
Joita Razvan-Gabriel <joitarazvan@yahoo.com>
2023-12-04 13:32:44 +0200

    1

--- src/test/java/org/example/service/StudentServiceTest.java
+++ src/test/java/org/example/service/StudentServiceTest.java
@@ -3,9 +3,11 @@ package org.example.service;
 import org.example.model.Students;
 import org.example.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -17,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock
