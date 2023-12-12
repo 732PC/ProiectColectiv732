f6c5cbbcb23d3bba3c80a43331df0583671c183a
Cleydy-Nicolas Toma-Micle <103383948+Kl3igrec@users.noreply.github.com>
2023-12-12 11:50:54 +0200

    Reformat code, delete unused Endpoint

--- src/main/java/org/example/model/Students.java
+++ src/main/java/org/example/model/Students.java
@@ -1,13 +1,17 @@
 package org.example.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.NoArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
-
 
 import java.time.LocalDate;
 
