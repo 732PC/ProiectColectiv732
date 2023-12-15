e3dcb486e2e05c1b1b419e5518811024533e898b
MihAMi02 <mihai.mihalache@stud.ubbcluj.ro>
2023-12-15 11:41:47 +0200

    all good

--- src/main/java/org/example/controller/CourseController.java
+++ src/main/java/org/example/controller/CourseController.java
@@ -38,7 +38,7 @@ public class CourseController {
     public ResponseEntity<String> addCourses(@RequestParam List<String> ids, @RequestParam String email){
         boolean successful = this.courseService.addListOfCourses(ids, email);
         if(successful){
-            return new ResponseEntity<>(null, HttpStatus.OK);
+            return new ResponseEntity<>("All good!", HttpStatus.OK);
         }
         else{
             return new ResponseEntity<>(null, HttpStatus.NOT_IMPLEMENTED);
