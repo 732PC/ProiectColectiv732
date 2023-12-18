119d312d4cb86bda117afc47079e56816e6ec2de
ChasmY <claudiusm124@gmail.com>
2023-12-18 13:43:21 +0200

    Functionalitati

--- src/main/java/org/example/service/AttendanceService.java
+++ src/main/java/org/example/service/AttendanceService.java
@@ -24,7 +24,6 @@ public class AttendanceService {
         return attendanceRepository.save(studentCourse);
     }
 
-    //Com ca sa ia pushul
     public StudentCourse addAttendance(StudentCourse studentCourse, Attendance attendance){
         studentCourse.setAttendance(attendance);
         return attendanceRepository.save(studentCourse);
